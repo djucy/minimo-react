0833d84fa481eff46f5022157b031330c144169d
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2022-09-06 12:52:18 +0300

    fix padding in the sidebar container

--- src/components/SinglePost/HeroImage/HeroImage.jsx
+++ src/components/SinglePost/HeroImage/HeroImage.jsx
@@ -1,8 +1,10 @@
 import { images } from 'images';
+import style from './HeroImage.module.css';
 
 export default function HeroImage() {
   return (
     <img
+      className={style.hero__img}
       src={images.images_articles.hero}
       alt="Two women standing in the desert"
     ></img>
